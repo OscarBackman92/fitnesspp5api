cf028d58ba4e6eed29736ac296a425cdbfb34c21
OscarBackman92 <jan.oscar.backman@gmail.com>
2024-11-12 20:49:36 +0100

    refactor: Socials urls.py

--- social/urls.py
+++ social/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from . import views
+from .views import UserFollowViewSet, WorkoutLikeViewSet, WorkoutCommentViewSet
 
 router = DefaultRouter()
-router.register(r'feed', views.SocialFeedViewSet, basename='social-feed')
-router.register(r'follow', views.UserFollowViewSet, basename='follow')
-router.register(r'likes', views.WorkoutLikeViewSet, basename='like')
-router.register(r'comments', views.WorkoutCommentViewSet, basename='comment')
+router.register(r'follow', UserFollowViewSet, basename='userfollow')
+router.register(r'likes', WorkoutLikeViewSet, basename='workoutlike')
+router.register(r'comments', WorkoutCommentViewSet, basename='workoutcomment')
 
 urlpatterns = [
     path('', include(router.urls)),
